436d23c8f75edbe40729343e13b477dec60980f0
Admin <Admin@TEB106008>
2023-01-27 11:03:32 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] M1 = new string[3];
+
+M1[0] = "Hej jag heter...?";
+M1[1] = "Hej jag kommer från...?";
+M1[2] = "Hej jag bor i...?";
+
+for (int i = 0; i < M1.Length; i++)
+{
+    Console.WriteLine(M1[i]);
+    Console.WriteLine();
+}
